112c698782ba13f257d3645b8e46b45d1abae60d
Jonathan Q <jonathan.quekww@gmail.com>
2024-02-02 20:22:11 +0800

    included user.js model

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 6;
+
+const userSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      required: true,
+    },
+    //to be reaccessed after S3
+    profilepic: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
+module.exports = model("User", userSchema);
