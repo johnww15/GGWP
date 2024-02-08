b3609313709d72f6aa8fa49fa4767c58803c2e3d
Jonathan Q <jonathan.quekww@gmail.com>
2024-02-08 18:19:54 +0800

    included postform and profile page CSS

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,36 +1,54 @@
-import { Container, TextField, Button } from "@mui/material";
+import { Box, TextField, Button } from "@mui/material";
 import { useState } from "react";
 
 export default function EntriesForm({ user }) {
   const [body, setBody] = useState("");
   const [multimedia, setMultimedia] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
   const handleSubmit = async () => {
     console.log("entriesform submit button clicked");
   };
 
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setBody(event.target.value);
+    if (newValue.length <= 300) {
+      setInputValue(newValue);
+    }
+  };
+
   return (
     <>
-      <Container sx={{ width: 350 }}>
+      <Box sx={{ width: "100%", padding: "5px" }}>
         <form autoComplete="off" onSubmit={handleSubmit}>
           <TextField
-            label="Post"
-            onChange={(e) => setBody(e.target.value)}
+            label="New Post"
+            onChange={handleChange}
             variant="outlined"
             color="secondary"
             type="body"
-            sx={{ mb: 3 }}
+            sx={{ mb: 1 }}
+            multiline
             fullWidth
+            maxRows={3}
+            helperText={`${inputValue.length}/300`}
+            inputProps={{ maxLength: 300 }}
             value={body}
           />
-          <Button variant="outlined" color="secondary" type="">
+          <Button
+            variant="outlined"
+            color="primary"
+            type=""
+            sx={{ marginRight: "5px" }}
+          >
             Add Multimedia
           </Button>
           <Button variant="outlined" color="primary" type="submit">
             Post it
           </Button>
         </form>
-      </Container>
+      </Box>
     </>
   );
 }
--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import Typography from "@mui/material/Typography";
 import PostForm from "../../components/PostForm";
+import { Box } from "@mui/material";
 
 export default function ProfilePage() {
   return (
@@ -7,7 +8,47 @@ export default function ProfilePage() {
       <Typography variant="h6" component="h1" sx={{ mb: 2 }}>
         Profile Page is running here
       </Typography>
-      <PostForm />
+      <Box
+        sx={{
+          border: "primary",
+          display: "flex",
+          flexDirection: "row",
+          overflow: "hidden",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            border: "2px solid",
+            borderColor: "primary",
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "space-around",
+            width: "30vh",
+            height: "100vh",
+            padding: "2px",
+            margin: "1px",
+          }}
+        >
+          <p>1st box</p>
+        </Box>
+        <Box
+          sx={{
+            border: "2px solid",
+            borderColor: "primary",
+            display: "flex",
+            flexDirection: "column",
+            width: "70vh",
+            height: "100vh",
+            padding: "2px",
+            margin: "1px",
+          }}
+        >
+          <PostForm />
+        </Box>
+      </Box>
     </>
   );
 }
