46c56c181f4b4b7122bc7dadc31c38932679335f
Jonathan Q <jonathan.quekww@gmail.com>
2024-02-14 01:08:58 +0800

    fixed handlechange and state for post update modal

--- src/components/FeedUpdateDialog.jsx
+++ src/components/FeedUpdateDialog.jsx
@@ -1,5 +1,7 @@
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 
 export default function FeedUpdateDialog({
   user,
@@ -7,21 +9,50 @@ export default function FeedUpdateDialog({
   handleUpdateClose,
   post,
 }) {
-  const username = post.userId.display_name;
   const content = post.content;
+  const [updatePostData, setUpdatePostData] = useState(post);
 
   const handleClose = () => {
     handleUpdateClose(post);
+    setUpdatePostData(post);
   };
+
+  const handleChange = (evt) => {
+    console.log(updatePostData);
+    setUpdatePostData({
+      ...updatePostData,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("update submit button clicked", updatePostData);
+  };
+
   return (
     <>
       <Dialog onClose={handleClose} open={updateOpen}>
         <DialogTitle>Set update form</DialogTitle>
-        <div>
-          <p>
-            {username} / {content}
-          </p>
-        </div>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <h2>Update Form</h2>
+          <TextField
+            label="content"
+            name="content"
+            defaultValue={content}
+            // value={updatePostData.content}
+            onChange={handleChange}
+            required
+            variant="outlined"
+            color="secondary"
+            type="content"
+            sx={{ mb: 3 }}
+            fullWidth
+          />
+        </form>
+        <Button variant="contained" color="primary">
+          Update
+        </Button>
       </Dialog>
     </>
   );
